e10297db4c294bbba2ea869c9a98ca5ba9efb7bd
Solleti Harshitha <solletiharshitha@gmail.com>
2021-08-05 17:10:23 +0530

    [Harshitha] Retrieving all the data

---
+++ AddressBookService/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AddressBookService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Address Book Service");
+            AddressBookRepository repository = new AddressBookRepository();
+            repository.GetAllEmployee();
+
+
+        }
+    }
+}
